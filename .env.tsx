7e355dcf8dfea5815736f2bc19caa6bb652189b7
ouratoken <ouratoken@gmail.com>
2026-01-06 22:38:07 +0530

    Create .env.tsx

---
+++ .env.tsx
@@ -0,0 +1,5 @@
+# MongoDB Atlas Connection String
+MONGODB_URI=mongodb+srv://oura_token:Ouratoken123@cluster0.wnedo7d.mongodb.net/oura-staking?retryWrites=true&w=majority&appName=Cluster0
+
+# Node Environment
+NODE_ENV=development
